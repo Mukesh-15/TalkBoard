c6c2c64d195b462d7ea58ef4fdb2fe71608ce225
Mukesh-15 <mukeshdoodapaka@gmail.com>
2026-06-11 17:00:32 +0530

    backend setup done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("working.....");
+const express = require('express');
+const app = express();
+const auth = require('./routes/auth');
+
+app.use('/auth', auth);
+app.get('/', (req, res) => {
+    console.log("working..");
+    res.send("working bruh");
+});
+
+app.listen(5000, () => {
+    console.log("Server runnig..");
+});
