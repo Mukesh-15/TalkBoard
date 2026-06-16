93b468aea21aebdd662156771295e0c51614a6d4
Mukesh-15 <mukeshdoodapaka@gmail.com>
2026-06-16 19:26:46 +0530

    client url stored in .env

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
